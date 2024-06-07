98c6c58d2b61889f90873448942a09833fcc47be
Stoyan Peev <stoyanilievpeev@gmail.com>
2024-06-07 15:28:24 +0100

    Fix undefined error in formatNumbers util function

--- src/utils/formatNumbers.ts
+++ src/utils/formatNumbers.ts
@@ -1,4 +1,7 @@
 export const formatNumbers = (numbers: number): string => {
+  if (numbers == null) {
+    return '0';
+  }
   if (numbers >= 1000) {
     return (numbers / 1000).toFixed(1) + 'k';
   }
